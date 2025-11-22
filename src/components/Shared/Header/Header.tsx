167dcbff2a41ea124fc3268acd2ae2433b1f3a59
sazeduzzaman <szamansaju@gmail.com>
2025-11-22 11:47:25 +0600

    file update

--- src/components/Shared/Header/Header.tsx
+++ src/components/Shared/Header/Header.tsx
@@ -23,7 +23,7 @@ export default async function Header() {
             </svg>
           </div>
           <ul
-            tabIndex="-1"
+            tabIndex={-1}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
