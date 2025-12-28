4a00f0a6cd69dedc28a71ff5eb2f564d1cc89f91
sazeduzzaman <szamansaju@gmail.com>
2025-12-28 21:55:19 +0600

    Some changes done

--- src/app/(main)/all-notice/page.tsx
+++ src/app/(main)/all-notice/page.tsx
@@ -1,7 +1,8 @@
 import NoticeBoard from "@/components/NoticeBoard/NoticeBoard";
 import { NoticesDataSet } from "@/lib/apis/NoticesDataSet/NoticesDataSet";
 
+// NoticeBoard Page Component
 export default async function Page() {
-  const noticesData = await NoticesDataSet(); 
+  const noticesData = await NoticesDataSet();
   return <NoticeBoard noticesData={noticesData} />;
 }
