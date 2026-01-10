3f136f8a800c398723501c7b16a2c9a69bb0dded
sazeduzzaman <szamansaju@gmail.com>
2026-01-10 23:04:54 +0600

    Some changes done

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true, // React Compiler optimizations
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*", // allows any HTTPS host
-      },
-    ],
-  },
   typescript: {
     // Optional: allow prod builds even if there are TS errors
     ignoreBuildErrors: false,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "admin.kau.khandkershahed.com",
+        pathname: "/**", // allow all images from this domain
+      },
+    ],
+  },
+  staticPageGenerationTimeout: 120, // ⬅️ increase build timeout to 120s
 };
 
 export default nextConfig;
